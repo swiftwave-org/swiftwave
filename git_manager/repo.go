79a398a58f76ca50656f8b3048b2f786db3e3dff
The-Debarghya <debarghya4444@gmail.com>
2023-08-21 11:30:42 +0530

    Change go-git to supported version

--- git_manager/repo.go
+++ git_manager/repo.go
@@ -5,10 +5,10 @@ import (
 	"log"
 	"os"
 
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	"gopkg.in/src-d/go-git.v4/plumbing/transport/http"
-	"gopkg.in/src-d/go-git.v4/storage/memory"
+	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/storage/memory"
 )
 
 // Fetch latest commit hash for a repository.
