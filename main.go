6a13e28752203e24b6fa093d0763ce22a2bbcffb
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-08-30 01:41:21 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Printl("Error loading .env file. Ignoring")
+		log.Println("Error loading .env file. Ignoring")
 	}
 	server := SERVER.Server{}
 	server.Init()
