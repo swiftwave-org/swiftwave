1a6ac7087dbcd51e926b99adc0be909c43b54acf
Tanmoy <ts741127@gmail.com>
2023-10-22 23:48:08 +0530

    chore: cleanup code

--- swiftwave_manager/core/models.go
+++ swiftwave_manager/core/models.go
@@ -26,86 +26,92 @@ type ImageRegistryCredential struct {
 type Domain struct {
 	ID            uint            `json:"id" gorm:"primaryKey"`
 	Name          string          `json:"name" gorm:"unique"`
-	SSLStatus     DomainSSLStatus `json:"ssl_status"`
-	SSLPrivateKey string          `json:"ssl_private_key"`
-	SSLFullChain  string          `json:"ssl_full_chain"`
-	SSLIssuedAt   time.Time       `json:"ssl_issued_at"`
-	SSLIssuer     string          `json:"ssl_issuer"`
-	IngressRules  []IngressRule   `json:"ingress_rules" gorm:"foreignKey:DomainID"`
-	RedirectRules []RedirectRule  `json:"redirect_rules" gorm:"foreignKey:DomainID"`
+	SSLStatus     DomainSSLStatus `json:"SSLStatus"`
+	SSLPrivateKey string          `json:"SSLPrivateKey"`
+	SSLFullChain  string          `json:"SSLFullChain"`
+	SSLIssuedAt   time.Time       `json:"SSLIssuedAt"`
+	SSLIssuer     string          `json:"SSLIssuer"`
+	IngressRules  []IngressRule   `json:"ingressRules" gorm:"foreignKey:DomainID"`
+	RedirectRules []RedirectRule  `json:"redirectRules" gorm:"foreignKey:DomainID"`
 }
 
 // IngressRule : hold information about Ingress rule for service
 type IngressRule struct {
 	ID            uint              `json:"id" gorm:"primaryKey"`
-	DomainID      uint              `json:"domain_id"`
-	ApplicationID string            `json:"application_id"`
+	Domain        Domain            `json:"-"`
+	DomainID      uint              `json:"domainID"`
+	Application   Application       `json:"-"`
+	ApplicationID string            `json:"applicationID"`
 	Protocol      ProtocolType      `json:"protocol"`
 	Port          uint              `json:"port"`
-	TargetPort    uint              `json:"target_port"`
+	TargetPort    uint              `json:"targetPort"`
 	Status        IngressRuleStatus `json:"status"`
-	CreatedAt     time.Time         `json:"created_at"`
-	UpdatedAt     time.Time         `json:"updated_at"`
+	CreatedAt     time.Time         `json:"createdAt"`
+	UpdatedAt     time.Time         `json:"updatedAt"`
 }
 
 // RedirectRule : hold information about Redirect rules for domain
 type RedirectRule struct {
 	ID          uint               `json:"id" gorm:"primaryKey"`
-	DomainID    uint               `json:"domain_id"`
+	Domain      Domain             `json:"-"`
+	DomainID    uint               `json:"domainID"`
 	Port        uint               `json:"port"`
-	RedirectURL string             `json:"redirect_url"`
+	RedirectURL string             `json:"redirectURL"`
 	Status      RedirectRuleStatus `json:"status"`
-	CreatedAt   time.Time          `json:"created_at"`
-	UpdatedAt   time.Time          `json:"updated_at"`
+	CreatedAt   time.Time          `json:"createdAt"`
+	UpdatedAt   time.Time          `json:"updatedAt"`
 }
 
 // Application : hold information about application
 type Application struct {
 	ID                   string `json:"id" gorm:"primaryKey"`
-	ServiceName          string `json:"service_name" gorm:"unique"`
-	EnvironmentVariables string `json:"environment_variables"` // JSON string
-	Volumes              string `json:"volumes"`               // JSON string
+	ServiceName          string `json:"serviceName" gorm:"unique"`
+	EnvironmentVariables string `json:"environmentVariables"` // JSON string
+	Volumes              string `json:"volumes"`              // JSON string
 	// Deployment
 	Deployments []Deployment `json:"deployments" gorm:"foreignKey:ApplicationID"`
 	// Ingress Rules
-	IngressRules []IngressRule `json:"ingress_rules" gorm:"foreignKey:ApplicationID"`
+	IngressRules []IngressRule `json:"ingressRules" gorm:"foreignKey:ApplicationID"`
 }
 
 // Deployment : hold information about deployment of application
 type Deployment struct {
 	ID            string       `json:"id" gorm:"primaryKey"`
-	ApplicationID uint         `json:"application_id"`
-	UpstreamType  UpstreamType `json:"upstream_type"`
+	Application   Application  `json:"-"`
+	ApplicationID uint         `json:"applicationID"`
+	UpstreamType  UpstreamType `json:"upstreamType"`
 	// Fields for UpstreamType = Git
-	GitCredentialID  uint        `json:"git_credential_id"`
-	GitProvider      GitProvider `json:"git_provider"`
-	RepositoryOwner  string      `json:"repository_owner"`
-	RepositoryName   string      `json:"repository_name"`
-	RepositoryBranch string      `json:"repository_branch"`
-	CommitHash       string      `json:"commit_hash"`
+	GitCredential    GitCredential `json:"-"`
+	GitCredentialID  uint          `json:"gitCredentialID"`
+	GitProvider      GitProvider   `json:"gitProvider"`
+	RepositoryOwner  string        `json:"repositoryOwner"`
+	RepositoryName   string        `json:"repositoryName"`
+	RepositoryBranch string        `json:"repositoryBranch"`
+	CommitHash       string        `json:"commitHash"`
 	// Fields for UpstreamType = SourceCode
-	SourceCodeCompressedFileName string `json:"source_code_compressed_file_name"`
+	SourceCodeCompressedFileName string `json:"sourceCodeCompressedFileName"`
 	// Fields for UpstreamType = Image
-	DockerImage               string `json:"docker_image"`
-	ImageRegistryCredentialID uint   `json:"image_registry_credential_id"`
+	DockerImage               string `json:"dockerImage"`
+	ImageRegistryCredentialID uint   `json:"ImageRegistryCredentialID"`
 	// Common Fields
 	BuildArgs  string `json:"build_args"` // JSON string
 	Dockerfile string `json:"dockerfile"`
 	// No of replicas to be deployed
-	DeploymentMode DeploymentMode `json:"deployment_mode"`
+	DeploymentMode DeploymentMode `json:"deploymentMode"`
 	Replicas       uint           `json:"replicas"`
 	// Logs
 	Logs []DeploymentLog `json:"logs" gorm:"foreignKey:DeploymentID"`
 	// Deployment Status
 	Status DeploymentStatus `json:"status"`
 	// Created At
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"createdAt"`
 }
 
 // DeploymentLog : hold logs of deployment
 type DeploymentLog struct {
-	ID           uint      `json:"id" gorm:"primaryKey"`
-	DeploymentID string    `json:"deployment_id"`
-	Content      string    `json:"content"`
-	CreatedAt    time.Time `json:"created_at"`
+	ID           uint       `json:"id" gorm:"primaryKey"`
+	Deployment   Deployment `json:"-"`
+	DeploymentID string     `json:"deploymentID"`
+	Content      string     `json:"content"`
+	CreatedAt    time.Time  `json:"createdAt"`
 }
