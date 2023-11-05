e4b7eb9cf6f45d9e92d6af961662e978f60a933d
Tanmoy Sarkar <ts741127@gmail.com>
2023-11-05 17:05:28 +0530

    feat: cronjobs added

--- swiftwave_service/cronjob/types.go
+++ swiftwave_service/cronjob/types.go
@@ -2,9 +2,16 @@ package cronjob
 
 import (
 	"github.com/swiftwave-org/swiftwave/swiftwave_service/core"
+	"sync"
 )
+
+type CronJob interface {
+	Start(nowait bool)
+	Wait()
+}
 
 type Manager struct {
 	ServiceConfig  *core.ServiceConfig
 	ServiceManager *core.ServiceManager
+	wg             *sync.WaitGroup
 }
