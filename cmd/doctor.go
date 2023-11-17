5617a42f53a2e6d752db7bb877a80d1ff52ddfbf
Tanmoy Sarkar <ts741127@gmail.com>
2023-11-17 22:41:26 +0530

    feat: boilerplate added

---
+++ cmd/doctor.go
@@ -0,0 +1,21 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+func init() {
+	rootCmd.AddCommand(doctorCmd)
+}
+
+var doctorCmd = &cobra.Command{
+	Use:   "doctor",
+	Short: "Analyze and generate reports about the current system",
+	Long:  "Analyze and generate reports about the current system",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Check if config file exists
+		// Check if Docker is installed
+		// Check if Docker Unix socket is working
+		// Check if haproxy-service is running
+		// Check if haproxy socket is working
+		// Check if haproxy authentication is working
+	},
+}
