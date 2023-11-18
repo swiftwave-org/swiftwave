34d382a558f836ae61d1067077c3430814edf740
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-11-18 21:21:17 +0000

    chore: cmd rename

--- cmd/haproxy.go
+++ cmd/haproxy.go
@@ -6,15 +6,15 @@ import "github.com/spf13/cobra"
 Create the HAProxy services across all the manager nodes
 ---
 Commands to run:
-swiftwave haproxy up
-swiftwave haproxy down
+swiftwave haproxy start
+swiftwave haproxy stop
 swiftwave haproxy status
 ---
 */
 
 func init() {
-	haproxyCmd.AddCommand(haproxyUpCmd)
-	haproxyCmd.AddCommand(haproxyDownCmd)
+	haproxyCmd.AddCommand(haproxyStartCmd)
+	haproxyCmd.AddCommand(haproxyStopCmd)
 	haproxyCmd.AddCommand(haproxyStatusCmd)
 	rootCmd.AddCommand(haproxyCmd)
 }
@@ -26,23 +26,23 @@ var haproxyCmd = &cobra.Command{
 }
 
 
-// Up command
-var haproxyUpCmd = &cobra.Command{
-	Use:   "up",
-	Short: "Create HAProxy service",
-	Long:  "Create HAProxy service",
+// Start command
+var haproxyStartCmd = &cobra.Command{
+	Use:   "start",
+	Short: "Start HAProxy service",
+	Long:  "Start HAProxy service",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Create HAProxy service
+		// Start HAProxy service
 	},
 }
 
-// Down command
-var haproxyDownCmd = &cobra.Command{
-	Use:   "down",
-	Short: "Remove HAProxy service",
-	Long:  "Remove HAProxy service",
+// Stop command
+var haproxyStopCmd = &cobra.Command{
+	Use:   "stop",
+	Short: "Stop HAProxy service",
+	Long:  "Stop HAProxy service",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Remove HAProxy service
+		// Stop HAProxy service
 	},
 }
 
