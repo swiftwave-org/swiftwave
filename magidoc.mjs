95a91dd42fa25cca30b96de5f7fba5a75e7ce293
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2023-11-24 03:11:13 +0530

    feat: minimum magidocs configuration added

---
+++ magidoc.mjs
@@ -0,0 +1,14 @@
+export default {
+    introspection: {
+      type: 'sdl',
+      paths: ['swiftwave_service/graphql/schema/**/*.graphqls'],
+    },
+    website: {
+        template: 'carbon-multi-page',
+        output: './graphql-docs',
+        options: {
+            appTitle: 'Swiftwave GraphQL Documentation',
+            appLogo: 'https://github.com/swiftwave-org.png',
+        }
+    }
+  }
