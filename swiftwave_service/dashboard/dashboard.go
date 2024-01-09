95bc68d055112411aebb87466d2f1a0c08f9c522
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-01-09 19:31:57 +0530

    fix: SPA 404 error fixed

--- swiftwave_service/dashboard/dashboard.go
+++ swiftwave_service/dashboard/dashboard.go
@@ -3,6 +3,9 @@ package dashboard
 import (
 	"embed"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+	"strings"
 )
 
 var (
@@ -15,6 +18,16 @@ var (
 )
 
 func RegisterHandlers(e *echo.Echo) {
+	e.Use(middleware.StaticWithConfig(middleware.StaticConfig{
+		Skipper: func(c echo.Context) bool {
+			// if request path doesn't start with /dashboard, skip
+			return !strings.HasPrefix(c.Request().URL.Path, "/dashboard")
+		},
+		Index:      "index.html",
+		Browse:     false,
+		HTML5:      true,
+		Filesystem: http.FS(distDirFS),
+	}))
 	e.FileFS("/dashboard", "index.html", distIndexHtml)
 	e.StaticFS("/dashboard", distDirFS)
 	// Re-direct / to /dashboard
