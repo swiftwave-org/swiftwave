38d9176345be6f0d581615021d319fc947ffc0a8
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-01-17 02:36:22 +0530

    feat: retry for 5 times for createDbClient function

--- swiftwave_service/core/create_db_client.go
+++ swiftwave_service/core/create_db_client.go
@@ -1,18 +1,31 @@
 package core
 
 import (
+	"errors"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
+	"time"
 )
 
 func createDbClient(dsn string) (*gorm.DB, error) {
 	// Initiating database client
 	dbDialect := postgres.Open(dsn)
-	client, err := gorm.Open(dbDialect, &gorm.Config{
-		SkipDefaultTransaction: true,
-	})
-	if err != nil {
-		return nil, err
+	maxAttempts := 5
+	for i := 0; i < maxAttempts; i++ {
+		dbClient, err := gorm.Open(dbDialect, &gorm.Config{
+			SkipDefaultTransaction: true,
+		})
+		if err != nil {
+			if i == maxAttempts-1 {
+				return nil, err
+			}
+			log.Println("Failed to connect to database. Retrying in 10 seconds...")
+			time.Sleep(10 * time.Second)
+			continue
+		}
+		return dbClient, nil
 	}
-	return client, nil
+	log.Println("Failed to connect to database. Retried 5 times. Exiting...")
+	return nil, errors.New("failed to connect to database")
 }
