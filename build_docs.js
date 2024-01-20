7772bc7e299b30dee4568503f35a52613c72f70d
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-01-20 21:23:55 +0530

    fix: add welcome page in magidoc

--- build_docs.js
+++ build_docs.js
@@ -153,7 +153,28 @@ function generateMjs(branchName) {
             options: {
                 siteRoot: '/${branchName}',
                 appTitle: 'Swiftwave GraphQL Documentation',
-                appLogo: 'https://github.com/swiftwave-org.png'
+                appLogo: 'https://github.com/swiftwave-org.png',
+                siteMeta: {
+                    description: "Documentation for Swiftwave's GraphQL API.",
+                    'og:description': "Documentation for Swiftwave's GraphQL API.",
+                  },
+                  pages: [
+                    {
+                      title: 'Welcome',
+                      content: markdown\`
+                        # 👋 Hi
+            
+                        Welcome to the documentation of the Swiftwave GraphQL API.
+      
+                        ## Don't know about Swiftwave?
+      
+                        SwiftWave is a self-hosted lightweight PaaS solution to deploy and manage your applications on any VPS without any hassle 👀
+                      
+                        ## Want to support us?
+                        Star ⭐ our [GitHub repository](https://github.com/swiftwave-org/swiftwave) and join our [Slack community](https://join.slack.com/t/swiftwave-team/shared_invite/zt-21n86aslx-aAvBi3hv1GigVA_XoXiu4Q) to get help and support from our team.
+                        \`,
+                    }
+                  ],
             }
         }
     }`
