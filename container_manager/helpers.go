64685ec94e6f9f7d56f2971d87276a700e63bf3d
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-02-14 03:39:02 +0530

    feat: volume-kit functions added

--- container_manager/helpers.go
+++ container_manager/helpers.go
@@ -1,7 +1,10 @@
 package containermanger
 
 import (
+	"fmt"
 	"github.com/docker/docker/api/types/registry"
+	"io"
+	"os"
 )
 
 func generateAuthHeader(username string, password string) (string, error) {
@@ -13,4 +16,36 @@ func generateAuthHeader(username string, password string) (string, error) {
 		Password: password,
 	}
 	return registry.EncodeAuthConfig(authConfig)
+}
+
+// copyFile : Copy a file from source to destination
+func copyFile(source, destination string) error {
+	sourceFile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer func(sourceFile *os.File) {
+		err := sourceFile.Close()
+		if err != nil {
+			fmt.Println("failed to close source file " + err.Error())
+		}
+	}(sourceFile)
+
+	destinationFile, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer func(destinationFile *os.File) {
+		err := destinationFile.Close()
+		if err != nil {
+			fmt.Println("failed to close destination file " + err.Error())
+		}
+	}(destinationFile)
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- container_manager/volume.go
+++ container_manager/volume.go
@@ -2,8 +2,18 @@ package containermanger
 
 import (
 	"errors"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/api/types/volume"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 )
+
+var volumeToolkitImage = "ghcr.io/swiftwave-org/volume-toolkit:latest"
 
 // CreateVolume : Create a new volume, return id of the volume
 func (m Manager) CreateVolume(name string) error {
@@ -42,4 +52,164 @@ func (m Manager) FetchVolumes() ([]string, error) {
 		volumeNames[i] = v.Name
 	}
 	return volumeNames, nil
+}
+
+// SizeVolume : Get the size of a volume in MB
+func (m Manager) SizeVolume(volumeName string) (sizeMB float64, err error) {
+	path, err := m.volumeToolkitRunner(volumeName, "size", nil, false)
+	if err != nil {
+		return 0, err
+	}
+	defer func(path string) {
+		err := os.RemoveAll(path)
+		if err != nil {
+			fmt.Println("failed to remove temp directory " + err.Error())
+		}
+	}(path)
+	resultPath := filepath.Join(path, "size.txt")
+	size, err := os.ReadFile(resultPath)
+	if err != nil {
+		return 0, errors.New("failed to read size file " + err.Error())
+	}
+	sizeBytes := 0
+	_, err = fmt.Sscanf(string(size), "%d", &sizeBytes)
+	if err != nil {
+		return 0, errors.New("failed to parse size " + err.Error())
+	}
+	sizeMB = float64(sizeBytes) / 1024 / 1024
+	return sizeMB, nil
+}
+
+// BackupVolume : Backup a volume to a file
+func (m Manager) BackupVolume(volumeName string, backupFilePath string) error {
+	if !strings.HasSuffix(backupFilePath, ".tar.gz") {
+		return errors.New("backupFilePath should have .tar.gz extension")
+	}
+	path, err := m.volumeToolkitRunner(volumeName, "export", nil, false)
+	if err != nil {
+		return err
+	}
+	defer func(path string) {
+		err := os.RemoveAll(path)
+		if err != nil {
+			fmt.Println("failed to remove temp directory " + err.Error())
+		}
+	}(path)
+	dumpFilePath := filepath.Join(path, "dump.tar.gz")
+	if err != nil {
+		return errors.New("failed to change permission of dump file " + err.Error())
+	}
+	// copy the backup file to the backupFilePath
+	err = copyFile(dumpFilePath, backupFilePath)
+	if err != nil {
+		return errors.New("failed to move backup file " + err.Error())
+	}
+	// make the backup file read only
+	err = os.Chmod(backupFilePath, 0666)
+	if err != nil {
+		// delete the backup file
+		_ = os.Remove(backupFilePath)
+		return errors.New("failed to change permission of dump file " + err.Error())
+	}
+	return nil
+}
+
+// RestoreVolume : Restore a volume from a backup file
+func (m Manager) RestoreVolume(volumeName string, backupFilePath string) error {
+	if !strings.HasSuffix(backupFilePath, ".tar.gz") {
+		return errors.New("backupFilePath should have .tar.gz extension")
+	}
+	// copy the backup file to a temp directory
+	outputPath, err := os.MkdirTemp("", "swiftwave-volume-toolkit-restore-*")
+	if err != nil {
+		return errors.New("failed to create temp directory " + err.Error())
+	}
+	dumpFilePath := filepath.Join(outputPath, "dump.tar.gz")
+	err = copyFile(backupFilePath, dumpFilePath)
+	if err != nil {
+		return errors.New("failed to move backup file " + err.Error())
+	}
+	// run the volume toolkit
+	path, err := m.volumeToolkitRunner(volumeName, "import", &outputPath, true)
+	if err != nil {
+		return err
+	}
+	defer func(path string) {
+		err := os.RemoveAll(path)
+		if err != nil {
+			fmt.Println("failed to remove temp directory " + err.Error())
+		}
+	}(path)
+	return nil
+}
+
+// private  function
+
+// volumeToolkitRunner : Run the volume toolkit container
+func (m Manager) volumeToolkitRunner(volumeName string, command string, predefinedOutputPath *string, dataDirectoryRW bool) (string, error) {
+	// check if volume exists
+	if !m.ExistsVolume(volumeName) {
+		return "", errors.New("volume does not exist")
+	}
+
+	// pull image if not exists
+	if !m.ExistsImage(volumeToolkitImage) {
+		resReader, err := m.client.ImagePull(m.ctx, volumeToolkitImage, types.ImagePullOptions{})
+		if err != nil {
+			return "", errors.New("failed to pull image " + err.Error())
+		}
+		// read the response but ignore it
+		_, err = io.Copy(io.Discard, resReader)
+	}
+	// create temp directory
+	if predefinedOutputPath == nil {
+		outputPath, err := os.MkdirTemp("", "swiftwave-volume-toolkit-*")
+		if err != nil {
+			return "", errors.New("failed to create temp directory " + err.Error())
+		}
+		predefinedOutputPath = &outputPath
+	}
+	var binds []string
+	if dataDirectoryRW {
+		binds = []string{fmt.Sprintf("%s:/data:rw", volumeName), fmt.Sprintf("%s:/app:rw", *predefinedOutputPath)}
+	} else {
+		binds = []string{fmt.Sprintf("%s:/data:ro", volumeName), fmt.Sprintf("%s:/app:rw", *predefinedOutputPath)}
+	}
+
+	createRes, err := m.client.ContainerCreate(m.ctx, &container.Config{
+		Image:           volumeToolkitImage,
+		AttachStdin:     false,
+		AttachStdout:    true,
+		AttachStderr:    true,
+		Tty:             false,
+		Cmd:             []string{command},
+		NetworkDisabled: true,
+	}, &container.HostConfig{
+		AutoRemove:  true,
+		NetworkMode: network.NetworkNone,
+		Privileged:  false,
+		Binds:       binds,
+	}, nil, nil, "")
+	if err != nil {
+		return "", errors.New("failed to create container " + err.Error())
+	}
+	// start the container
+	err = m.client.ContainerStart(m.ctx, createRes.ID, container.StartOptions{})
+	if err != nil {
+		return "", errors.New("failed to start container " + err.Error())
+	}
+
+	// wait for the container to finish
+	waitRes, waitErr := m.client.ContainerWait(m.ctx, createRes.ID, container.WaitConditionRemoved)
+	for {
+		select {
+		case err := <-waitErr:
+			return "", errors.New("failed to wait for container " + err.Error())
+		case res := <-waitRes:
+			if res.Error != nil {
+				return "", errors.New("container error " + res.Error.Message)
+			}
+			return *predefinedOutputPath, nil
+		}
+	}
 }
