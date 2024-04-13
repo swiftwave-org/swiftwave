dfdcfb58a7ce6d326a4ab3f90234b455dcadee65
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-04-14 01:29:56 +0530

    feat: ci action added to track uncommited db migration records

---
+++ .github/scripts/check_if_migration_missed.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# get output of atlas migrate diff --env gorm
+output=$(atlas migrate diff --env gorm)
+
+# check if output contains `no changes to be made`
+if echo "$output" | grep -q 'no changes to be made'; then
+    echo "No migration changes detected"
+    exit 0
+else
+    echo "Migration changes detected and need to be committed"
+    echo "Please run ./generate_migration_records.sh <name> to generate new migration files"
+    exit 1
+fi
