31b50bef7dab0678352a6c75eda5a0b2528e5c3e
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-04-14 01:06:53 +0530

    feat: embed fs support added for migration files

--- swiftwave_service/db/migrate_database.go
+++ swiftwave_service/db/migrate_database.go
@@ -1,15 +1,27 @@
 package db
 
 import (
+	"embed"
 	"errors"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/swiftwave-org/swiftwave/swiftwave_service/logger"
 	"gorm.io/gorm"
 )
 
+//go:embed all:migrations
+var migrationFilesFS embed.FS
+
 func MigrateDatabase(client *gorm.DB) error {
+	migrationFSDriver, err := iofs.New(migrationFilesFS, "migrations")
+	if err != nil {
+		logger.DatabaseLoggerError.Println("Failed to create migration fs driver")
+		logger.DatabaseLoggerError.Println(err)
+		return errors.New("unable to parse migration files")
+	}
+	// Create db connection
 	sqlDb, err := client.DB()
 	if err != nil {
 		logger.DatabaseLoggerError.Println("Failed to create migrate instance")
@@ -22,8 +34,9 @@ func MigrateDatabase(client *gorm.DB) error {
 		logger.DatabaseLoggerError.Println(err)
 		return errors.New("failed to migrate database")
 	}
-	m, err := migrate.NewWithDatabaseInstance(
-		"file://swiftwave_service/db/migrations",
+	m, err := migrate.NewWithInstance(
+		"iofs",
+		migrationFSDriver,
 		"postgres", driver)
 	if err != nil {
 		logger.DatabaseLoggerError.Println("Failed to create migrate instance")
