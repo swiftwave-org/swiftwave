2ab00b326f9fe9e419a37678c8f7149882921676
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-06-06 12:22:13 +0530

    feat: login route modified to support 2fa

--- go.mod
+++ go.mod
@@ -101,6 +101,7 @@ require (
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
 	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	github.com/xlzd/gotp v0.1.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.49.0 // indirect
 	go.opentelemetry.io/otel v1.24.0 // indirect
--- swiftwave_service/rest/auth.go
+++ swiftwave_service/rest/auth.go
@@ -3,6 +3,9 @@ package rest
 import (
 	"github.com/labstack/echo/v4"
 	"github.com/swiftwave-org/swiftwave/swiftwave_service/core"
+	"github.com/xlzd/gotp"
+	"strings"
+	"time"
 )
 
 // POST /auth/login
@@ -10,32 +13,62 @@ func (server *Server) login(c echo.Context) error {
 	// Get params
 	username := c.FormValue("username")
 	password := c.FormValue("password")
+	totp := c.FormValue("totp")
 
 	// Check if user exists
 	user, err := core.FindUserByUsername(c.Request().Context(), server.ServiceManager.DbClient, username)
 	if err != nil {
-		return c.JSON(400, map[string]string{
-			"message": "user does not exist",
+		return c.JSON(400, &LoginResponse{
+			Message:      "user does not exist",
+			Token:        "",
+			TotpRequired: false,
+		})
+	}
+
+	// check if 2fa is enabled
+	if user.TotpEnabled && strings.Compare(totp, "") != 0 {
+		return c.JSON(400, &LoginResponse{
+			Message:      "two factor authentication is enabled, but totp is not provided",
+			Token:        "",
+			TotpRequired: true,
 		})
 	}
 
 	// Check password
 	if !user.CheckPassword(password) {
-		return c.JSON(400, map[string]string{
-			"message": "incorrect password",
+		return c.JSON(400, &LoginResponse{
+			Message:      "invalid password",
+			Token:        "",
+			TotpRequired: false,
 		})
+	}
+
+	// Check 2fa
+	if user.TotpEnabled {
+		totpRecord := gotp.NewDefaultTOTP(user.TotpSecret)
+		if totpRecord.Verify(totp, time.Now().Unix()) == false {
+			return c.JSON(400, &LoginResponse{
+				Message:      "invalid two factor authentication code",
+				Token:        "",
+				TotpRequired: true,
+			})
+		}
 	}
 
 	// Generate jwt token
 	token, err := user.GenerateJWT(server.Config.SystemConfig.JWTSecretKey)
 	if err != nil {
-		return c.JSON(500, map[string]string{
-			"message": "failed to generate jwt token",
+		return c.JSON(500, &LoginResponse{
+			Message:      "failed to generate jwt token",
+			Token:        "",
+			TotpRequired: false,
 		})
 	}
 
 	// Return token
-	return c.JSON(200, map[string]string{
-		"token": token,
+	return c.JSON(200, &LoginResponse{
+		Message:      "success",
+		Token:        token,
+		TotpRequired: false,
 	})
 }
