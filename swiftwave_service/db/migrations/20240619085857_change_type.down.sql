f105f4f3c24bd669664454145c490cc4111858fd
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-06-19 16:16:08 +0530

    chore: update db migration

--- swiftwave_service/db/migrations/20240619085857_change_type.down.sql
+++
@@ -1,2 +0,0 @@
--- reverse: modify "applications" table
-ALTER TABLE "public"."applications" ALTER COLUMN "preferred_server_hostnames" TYPE character varying(254);
--- swiftwave_service/db/migrations/20240619085857_change_type.up.sql
+++
@@ -1,2 +0,0 @@
--- modify "applications" table
-ALTER TABLE "public"."applications" ALTER COLUMN "preferred_server_hostnames" TYPE text[];
