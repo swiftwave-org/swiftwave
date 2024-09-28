0dd1835d098dcd70e9d76cff589a9fbdb4c58918
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-09-28 16:39:37 +0530

    chore: modularize the MonitorServerStatus function

--- swiftwave_service/cronjob/server_status_monitor.go
+++ swiftwave_service/cronjob/server_status_monitor.go
@@ -2,18 +2,20 @@ package cronjob
 
 import (
 	"bytes"
+	"strings"
+	"sync"
+	"time"
+
 	"github.com/swiftwave-org/swiftwave/ssh_toolkit"
 	"github.com/swiftwave-org/swiftwave/swiftwave_service/core"
 	"github.com/swiftwave-org/swiftwave/swiftwave_service/logger"
-	"strings"
-	"time"
 )
 
 func (m Manager) MonitorServerStatus() {
 	logger.CronJobLogger.Println("Starting server status monitor [cronjob]")
 	for {
 		m.monitorServerStatus()
-		time.Sleep(1 * time.Minute)
+		time.Sleep(10 * time.Second)
 	}
 }
 
@@ -29,31 +31,39 @@ func (m Manager) monitorServerStatus() {
 	if len(servers) == 0 {
 		logger.CronJobLogger.Println("Skipping ! No server found")
 		return
+	}
+
+	var wg sync.WaitGroup
+	for _, server := range servers {
+		if server.Status == core.ServerNeedsSetup || server.Status == core.ServerPreparing {
+			continue
+		}
+		wg.Add(1)
+		go func(server core.Server) {
+			defer wg.Done()
+			m.checkAndUpdateServerStatus(server)
+		}(server)
+	}
+	wg.Wait()
+}
+
+func (m Manager) checkAndUpdateServerStatus(server core.Server) {
+	if server.Status == core.ServerOffline {
+		ssh_toolkit.DeleteSSHClient(server.HostName)
+	}
+	if m.isServerOnline(server) {
+		err := core.MarkServerAsOnline(&m.ServiceManager.DbClient, &server)
+		if err != nil {
+			logger.CronJobLoggerError.Println("DB Error : Failed to mark server as online >", server.HostName, err)
+		} else {
+			logger.CronJobLogger.Println("Server marked as online >", server.HostName)
+		}
 	} else {
-		for _, server := range servers {
-			if server.Status == core.ServerNeedsSetup || server.Status == core.ServerPreparing {
-				continue
-			}
-			go func(server core.Server) {
-				if server.Status == core.ServerOffline {
-					ssh_toolkit.DeleteSSHClient(server.HostName)
-				}
-				if m.isServerOnline(server) {
-					err = core.MarkServerAsOnline(&m.ServiceManager.DbClient, &server)
-					if err != nil {
-						logger.CronJobLoggerError.Println("DB Error : Failed to mark server as online > ", server.HostName)
-					} else {
-						logger.CronJobLogger.Println("Server marked as online > ", server.HostName)
-					}
-				} else {
-					err = core.MarkServerAsOffline(&m.ServiceManager.DbClient, &server)
-					if err != nil {
-						logger.CronJobLoggerError.Println("DB Error : Failed to mark server as offline > ", server.HostName)
-					} else {
-						logger.CronJobLogger.Println("Server marked as offline > ", server.HostName)
-					}
-				}
-			}(server)
+		err := core.MarkServerAsOffline(&m.ServiceManager.DbClient, &server)
+		if err != nil {
+			logger.CronJobLoggerError.Println("DB Error : Failed to mark server as offline >", server.HostName, err)
+		} else {
+			logger.CronJobLogger.Println("Server marked as offline >", server.HostName)
 		}
 	}
 }
@@ -66,6 +76,7 @@ func (m Manager) isServerOnline(server core.Server) bool {
 		stderrBuf := new(bytes.Buffer)
 		err := ssh_toolkit.ExecCommandOverSSH(cmd, stdoutBuf, stderrBuf, 3, server.IP, server.SSHPort, server.User, m.Config.SystemConfig.SshPrivateKey)
 		if err != nil {
+			time.Sleep(1 * time.Second)
 			continue
 		}
 		if strings.Compare(strings.TrimSpace(stdoutBuf.String()), "ok") == 0 {
